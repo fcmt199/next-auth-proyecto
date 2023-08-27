abba63366d49cb4750c661a10605de0d9527ccc1
Flavia Moscoso <flavia.celeste1999@gmail.com>
2023-08-27 17:40:30 -0400

    mi primer commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ function HomePage({session}) {
                         <img src={session.user.image} alt=""></img>
                     </div>
                 ):(
-                    <p>Skeleton</p>
+                    <p>Skeleton Flavia Moscoso</p>
                 )
             }
             <button onClick={()=> signOut()}>
